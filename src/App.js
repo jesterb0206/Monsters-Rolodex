425c75f5be214cb3cc3e90d0b443fbff7d62ddf5
jesterb0206 <jesterb@seattleu.edu>
2023-05-08 16:55:02 -0700

    States and Shallow Merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Rosa',
+      name: {firstName: 'Bradley', lastName: 'Jester'},
+      company: 'Meta',
     };
   }
 
@@ -16,10 +17,13 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>Hi {this.state.name}!</p>
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{' '}
+            {this.state.company}!
+          </p>
           <button
             onClick={() => {
-              this.setState({name: 'Bradley'});
+              this.setState({name: {firstName: 'Rosa', lastName: 'Ramirez'}});
             }}
           >
             Change Name
