db863b090157e0154db68bb1c100ce4275e9a499
jesterb0206 <jesterb@seattleu.edu>
2023-05-08 16:42:02 -0700

    Monsters Rolodex - setState

--- src/App.js
+++ src/App.js
@@ -17,7 +17,13 @@ class App extends Component {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>Hi {this.state.name}!</p>
-          <button>Change Name</button>
+          <button
+            onClick={() => {
+              this.setState({name: 'Bradley'});
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
