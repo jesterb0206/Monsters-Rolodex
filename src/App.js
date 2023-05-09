69eedde952dc6de0448d48017cb5ea7e6ccf34c5
jesterb0206 <jesterb@seattleu.edu>
2023-05-08 23:56:01 -0700

    Monsters Rolodex - Optimizations

--- src/App.js
+++ src/App.js
@@ -21,9 +21,19 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField};
+    });
+  };
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -32,12 +42,7 @@ class App extends Component {
           className='search-box'
           type='search'
           placeholder='Search for a Monster'
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return {searchField};
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
