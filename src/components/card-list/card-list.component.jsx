94e639c132646f03025c44e1ac9dd3839b6f162d
jesterb0206 <jesterb@seattleu.edu>
2023-05-09 23:14:01 -0700

    Monsters Rolodex - CardList Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,25 @@
 import {Component} from 'react';
+import './card-list.styles.css';
 
 class CardList extends Component {
   render() {
     const {monsters} = this.props;
 
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          const {name, email, id} = monster;
+          return (
+            <div className='card-container' key={id}>
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
