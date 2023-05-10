8f96853d955afc228a597a10726060252f5f06c7
jesterb0206 <jesterb@seattleu.edu>
2023-05-10 16:21:02 -0700

    Monsters Rolodex - Hooks: useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import {Component} from 'react';
+import {useState} from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
+  const [searchField, setSearchField] = useState('');
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className='App'>
       <h1 className='app-title'>Monsters Rolodex</h1>
-      {/* <SearchBox
+      <SearchBox
         className='monsters-search-box'
         onChangeHandler={onSearchChange}
         placeholder={'Search Monsters!'}
       />
-      <CardList monsters={filteredMonsters} /> */}
+
+      {/* <CardList monsters={filteredMonsters} /> */}
     </div>
   );
 };
